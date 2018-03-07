d5401af1b94695922a5872e29dc525f351c089dd
Kamil Kruczek <kamil.kruczek@grandparade.co.uk>
2018-03-07 16:55:50 +0100

    Initial works2

---
+++ src/test/unit/components/App/app.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+//
+// import App from '../../../../main/components/App';
+//
+// describe('<App />', () => {
+//     test('should render when required props are provided', () => {
+//         expect(shallow(<App />).exists()).toEqual(true);
+//     });
+// });
